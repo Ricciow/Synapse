eec3d3fb24beab4d0b0bdc4d3899dde35b9cc30f
CarolAtolini <carolineatolini@gmail.com>
2025-10-19 19:05:39 -0300

    Create ChatAnswerArea.tsx

---
+++ src/components/ChatPage/ChatAnswerArea.tsx
@@ -0,0 +1,26 @@
+import type { ModelProps } from '../Props.tsx';
+import '../../styles/components/ChatAnswerArea.css';
+
+export default function Chat({
+  modelName,
+  modelData,
+}: {
+  modelName: string;
+  modelData: ModelProps;
+}) {
+  const logo = modelData.logo;
+
+  return (
+    <div className="chat">
+      <div className="chat_header">
+        <input
+          type="checkbox"
+          className="model-checkbox"
+        />
+        <img src={logo} alt={modelName + ' logo'} />
+        <p>{modelName}</p>
+      </div>
+      <div className="chat_messages">mensagem mensagem mensagem</div>
+    </div>
+  );
+}
