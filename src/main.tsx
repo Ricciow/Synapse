3e328c6c8ede5bcf17d6f5c88283e7f9c26628dc
CarolAtolini <carolineatolini@gmail.com>
2025-10-19 19:21:53 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Index from '.'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+import Synapse from './pages/Synapse';
+import ChatPage from './pages/ChatPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    loader: () => {
+      return redirect('/synapse');
+    },
+  },
+  {
+    path: '/chat',
+    element: <ChatPage />,
+  },
+  {
+    path: '/synapse',
+    element: <Synapse />,
+  },
+  {
+    path: '*',
+    element: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Index />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
