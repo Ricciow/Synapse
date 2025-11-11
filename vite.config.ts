c05295df0c6a6a1f2e95693cda9cf50eb6e43be2
Riccio <riccio.fernando@gmail.com>
2025-11-11 13:51:41 -0300

    preparar para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+  preview: {
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: ["synapse.azurewebsites.net"],
+  },
 })
